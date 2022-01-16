1e4b70fbe7c7dfe5a27755eb40a518376f3d1744
Decio Neto <deciom.neto10@gmail.com>
2022-01-16 18:59:34 -0300

    feat: add sass

---
+++ src/components/Greeting.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+  export default {
+    name: 'Greeting',
+    data() {
+      return {
+        msg: 'Hello World',
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+$color: red;
+
+h1 {
+  color: $color;
+}
+</style>
