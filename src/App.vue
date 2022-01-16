2194daf0c10e7aa9c909a0721522a31d2d658e24
Decio Neto <deciom.neto10@gmail.com>
2022-01-16 19:15:50 -0300

    feat: add user component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <Greeting />
+  <User :age="age" />
 </template>
 
 <script>
+import Greeting from './components/Greeting.vue'
+import User from './components/User.vue'
+
 export default {
   name: 'App',
+  components: {
+    Greeting,
+    User,
+  },
+  data() {
+    return {
+      age: 20
+    }
+  }
 };
 </script>
